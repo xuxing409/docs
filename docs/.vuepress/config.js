09b44f5660491a273c9f4c0008cdfb973a4d3cff
Snxy <892573319@qq.com>
2022-07-31 20:08:12 +0800

    docs: 修改导航栏

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,36 +1,36 @@
 module.exports = {
-  title: 'star’s blog',
-  description: '了解真相,你才能获得更多的自由',
+  title: "star’s blog",
+  description: "了解真相,你才能获得更多的自由",
   themeConfig: {
-    logo: '/assets/img/logo.png',
     nav: [
-      { text: 'Home', link: '/' },
-     
+      { text: "Home", link: "/" },
+
       {
-        text: 'Vue',
-        items: [
-          {
-            text: 'Vue技术文档', items: [
-              { text: 'props传值更新', link: '/vue-01' },
-            ]
-          }
-        ]
+        text: "js",
+
       },
       {
-        text: '关于',
-        items: [
-          {
-            text: '关于我', items: [
-              { text: '自学前端心路历程', link: '/about' },
-            ]
-          }
-        ]
+        text: "vue",
+        // items: [
+        //   {
+        //     text: "Vue技术文档",
+        //     items: [{ text: "props传值更新", link: "/vue-01" }],
+        //   },
+        // ],
       },
-      { text: 'External', link: 'https://google.com' },
+      {
+        text: "react",
+      },
+      {
+        text: "关于",
+        // items: [
+        //   {
+        //     text: "关于我",
+        //     items: [{ text: "自学前端心路历程", link: "/about" }],
+        //   },
+        // ],
+      },
+      { text: "External", link: "https://google.com" },
     ],
-    // sidebar: [
-    //   '',
-    //   '/vue-01',
-    // ]
-  }
-}
+  },
+};
